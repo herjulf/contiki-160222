7bc1f2c6ae0f37c35b31304a680051a032c92aa9
Robert Olsson <robert@herjulf.se>
2015-12-22 23:58:29 +0100

    Reverting patch 	modified:   avr-rss2/contiki-conf.h

--- platform/avr-rss2/contiki-conf.h
+++ platform/avr-rss2/contiki-conf.h
@@ -118,6 +118,8 @@ void clock_adjust_ticks(clock_time_t howmany);
 
 /* Starting address for code received via the codeprop facility. Not tested. */
 typedef unsigned long off_t;
+/* #define EEPROMFS_ADDR_CODEPROP 0x8000 */
+
 /* Logging adds 200 bytes to program size. RS232 output slows down webserver. */
 /* #define LOG_CONF_ENABLED         1 */
 
@@ -178,8 +180,167 @@ typedef unsigned short uip_stats_t;
 #define NETSTACK_CONF_NETWORK     rime_driver
 #endif
 
+#define UIP_CONF_LL_802154        1
+#define UIP_CONF_LLH_LEN          0
+
+/* 10 bytes per stateful address context - see sicslowpan.c */
+/* Default is 1 context with prefix aaaa::/64 */
+/* These must agree with all the other nodes or there will be a failure to communicate! */
+#define SICSLOWPAN_CONF_MAX_ADDR_CONTEXTS 1
+#define SICSLOWPAN_CONF_ADDR_CONTEXT_0 { addr_contexts[0].prefix[0] = 0xaa; addr_contexts[0].prefix[1] = 0xaa; }
+#define SICSLOWPAN_CONF_ADDR_CONTEXT_1 { addr_contexts[1].prefix[0] = 0xbb; addr_contexts[1].prefix[1] = 0xbb; }
+#define SICSLOWPAN_CONF_ADDR_CONTEXT_2 { addr_contexts[2].prefix[0] = 0x20; addr_contexts[2].prefix[1] = 0x01; addr_contexts[2].prefix[2] = 0x49; addr_contexts[2].prefix[3] = 0x78, addr_contexts[2].prefix[4] = 0x1d; addr_contexts[2].prefix[5] = 0xb1; }
+
+/* Take the default TCP maximum segment size for efficiency and simpler wireshark captures */
+/* Use this to prevent 6LowPAN fragmentation (whether or not fragmentation is enabled) */
+/* #define UIP_CONF_TCP_MSS       48 */
+
+#define UIP_CONF_IP_FORWARD      0
+#define UIP_CONF_FWCACHE_SIZE    0
+
+#define UIP_CONF_IPV6_CHECKS     1
+#define UIP_CONF_IPV6_QUEUE_PKT  1
+#define UIP_CONF_IPV6_REASSEMBLY 0
+
+#define UIP_CONF_UDP_CHECKSUMS   1
+#define UIP_CONF_TCP_SPLIT       1
+#define UIP_CONF_DHCP_LIGHT      1
+
+#if 0 /* No radio cycling */
+
+#define NETSTACK_CONF_MAC         nullmac_driver
+#define NETSTACK_CONF_RDC         sicslowmac_driver
+#define NETSTACK_CONF_FRAMER      framer_802154
+/* 1 + Number of auto retry attempts 0-15 (0 implies don't use extended TX_ARET_ON mode) */
+#define RF230_CONF_FRAME_RETRIES    2
+/* Number of csma retry attempts 0-5 in extended tx mode (7 does immediate tx with no csma) */
+#define RF230_CONF_CSMA_RETRIES   5
+/* Default is one RAM buffer for received packets. More than one may benefit multiple TCP connections or ports */
+#define RF230_CONF_RX_BUFFERS     3
+#define SICSLOWPAN_CONF_FRAG      1
+/* Most browsers reissue GETs after 3 seconds which stops fragment reassembly so a longer MAXAGE does no good */
+#define SICSLOWPAN_CONF_MAXAGE    3
+/* How long to wait before terminating an idle TCP connection. Smaller to allow faster sleep. Default is 120 seconds */
+/* If wait is too short the connection can be reset as a result of multiple fragment reassembly timeouts */
+#define UIP_CONF_WAIT_TIMEOUT    20
+/* 54 bytes per queue ref buffer */
+#define QUEUEBUF_CONF_REF_NUM     2
+/* Allocate remaining RAM as desired */
+/* 30 bytes per TCP connection */
+/* 6LoWPAN does not do well with concurrent TCP streams, as new browser GETs collide with packets coming */
+/* from previous GETs, causing decreased throughput, retransmissions, and timeouts. Increase to study this. */
+/* ACKs to other ports become interleaved with computation-intensive GETs, so ACKs are particularly missed. */
+/* Increasing the number of packet receive buffers in RAM helps to keep ACKs from being lost */
+#define UIP_CONF_MAX_CONNECTIONS  4
+/* 2 bytes per TCP listening port */
+#define UIP_CONF_MAX_LISTENPORTS  4
+/* 25 bytes per UDP connection */
+#define UIP_CONF_UDP_CONNS       10
+/* See uip-ds6.h */
+#define NBR_TABLE_CONF_MAX_NEIGHBORS      20
+#define UIP_CONF_DS6_DEFRT_NBU    2
+#define UIP_CONF_DS6_PREFIX_NBU   3
+#define UIP_CONF_MAX_ROUTES    20
+#define UIP_CONF_DS6_ADDR_NBU     3
+#define UIP_CONF_DS6_MADDR_NBU    0
+#define UIP_CONF_DS6_AADDR_NBU    0
+
+#elif 1  /* Contiki-mac radio cycling */
+/* #define NETSTACK_CONF_MAC         nullmac_driver */
+/* csma needed for burst mode at present. Webserver won't work without it */
+/* #define NETSTACK_CONF_MAC         csma_driver */
+/* #define NETSTACK_CONF_RDC         contikimac_driver */
+/* Default is two CCA separated by 500 usec */
+
+/* So without the header this needed for RPL mesh to form */
+#define CONTIKIMAC_FRAMER_CONF_SHORTEST_PACKET_SIZE   43 - 18  /* multicast RPL DIS length */
+/* Not tested much yet */
+#define CONTIKIMAC_CONF_WITH_PHASE_OPTIMIZATION 0
+#define CONTIKIMAC_CONF_COMPOWER               1
+#define RIMESTATS_CONF_ENABLED                 0
+
+/* A 0 here means non-extended mode; 1 means extended mode with no retry, >1 for retrys */
+/* Contikimac strobes on its own, but hardware retries are faster */
+#define RF230_CONF_FRAME_RETRIES  1
+/* Long csma backoffs will compromise radio cycling; set to 0 for 1 csma */
+#define RF230_CONF_CSMA_RETRIES   0
+#define SICSLOWPAN_CONF_FRAG      1
+#define SICSLOWPAN_CONF_MAXAGE    3
+/* 54 bytes per queue ref buffer */
+#define QUEUEBUF_CONF_REF_NUM     2
+/* Allocate remaining RAM. Not much left due to queuebuf increase  */
+#define UIP_CONF_MAX_CONNECTIONS  2
+#define UIP_CONF_MAX_LISTENPORTS  4
+#define UIP_CONF_UDP_CONNS        5
+#define NBR_TABLE_CONF_MAX_NEIGHBORS      20
+#define UIP_CONF_DS6_DEFRT_NBU    2
+#define UIP_CONF_DS6_PREFIX_NBU   3
+#define UIP_CONF_MAX_ROUTES    4
+#define UIP_CONF_DS6_ADDR_NBU     3
+#define UIP_CONF_DS6_MADDR_NBU    0
+#define UIP_CONF_DS6_AADDR_NBU    0
+
+#elif 0  /* cx-mac radio cycling */
+/* RF230 does clear-channel assessment in extended mode (autoretries>0) */
+/* These values are guesses */
+#define RF230_CONF_FRAME_RETRIES  10
+#define RF230_CONF_CSMA_RETRIES   2
+#if RF230_CONF_CSMA_RETRIES
+#define NETSTACK_CONF_MAC         nullmac_driver
+#else
+#define NETSTACK_CONF_MAC         csma_driver
+#endif
+#define NETSTACK_CONF_RDC         cxmac_driver
+#define NETSTACK_CONF_FRAMER      framer_802154
+#define SICSLOWPAN_CONF_FRAG      1
+#define SICSLOWPAN_CONF_MAXAGE    3
+#define CXMAC_CONF_ANNOUNCEMENTS  0
+/* 54 bytes per queue ref buffer */
+#define QUEUEBUF_CONF_REF_NUM     2
+/* Allocate remaining RAM. Not much left due to queuebuf increase  */
+#define UIP_CONF_MAX_CONNECTIONS  2
+#define UIP_CONF_MAX_LISTENPORTS  4
+#define UIP_CONF_UDP_CONNS        5
+#define NBR_TABLE_CONF_MAX_NEIGHBORS      4
+#define UIP_CONF_DS6_DEFRT_NBU    2
+#define UIP_CONF_DS6_PREFIX_NBU   3
+#define UIP_CONF_MAX_ROUTES    4
+#define UIP_CONF_DS6_ADDR_NBU     3
+#define UIP_CONF_DS6_MADDR_NBU    0
+#define UIP_CONF_DS6_AADDR_NBU    0
+/* Below gives 10% duty cycle, undef for default 5% */
+/* #define CXMAC_CONF_ON_TIME (RTIMER_ARCH_SECOND / 80) */
+/* Below gives 50% duty cycle */
+/* #define CXMAC_CONF_ON_TIME (RTIMER_ARCH_SECOND / 16) */
+
+#else
+/* #error Network configuration not specified! */
+#endif /* Network setup */
+
+#ifndef QUEUEBUF_CONF_NUM
+#define QUEUEBUF_CONF_NUM         15
+#endif
+
+/* ************************************************************************** */
+/* #pragma mark RPL Settings */
+/* ************************************************************************** */
 #if UIP_CONF_IPV6_RPL
+
+#define UIP_CONF_ROUTER                 1
+#define UIP_CONF_ND6_SEND_RA        0
+#define UIP_CONF_ND6_REACHABLE_TIME     600000
+#define UIP_CONF_ND6_RETRANS_TIMER      10000
+
+/* For slow slip connections, to prevent buffer overruns */
+/* #define UIP_CONF_RECEIVE_WINDOW 300 */
+#undef UIP_CONF_FWCACHE_SIZE
 #define UIP_CONF_BUFFER_SIZE   600 /*  DHCPv4 packets by ip64 module */
+#define UIP_CONF_FWCACHE_SIZE    30
+#define UIP_CONF_BROADCAST       1
+#define UIP_ARCH_IPCHKSUM        1
+#define UIP_CONF_PINGADDRCONF    0
+#define UIP_CONF_LOGGING         0
+
 #endif /* RPL */
 
 #define CCIF
